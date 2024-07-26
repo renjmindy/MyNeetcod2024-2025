a56f1e999648712dd69433687391b491d0629b87
Mindy Jen <jencmhep@gmail.com>
2024-07-26 03:16:16 -0400

    Update Minimum_Changes_To_Make_Alternating_Binary_String.py

--- Easy/Arrays_Hashing/Minimum_Changes_To_Make_Alternating_Binary_String.py
+++ Easy/Arrays_Hashing/Minimum_Changes_To_Make_Alternating_Binary_String.py
@@ -1,10 +1,9 @@
 class Solution:
     def minOperations(self, s: str) -> int:
         
-        anse, anso = 0, 0
+        ans = 0
 
         for i, c in enumerate(s):
-            if i % 2 == int(c): anse += 1
-            else: anso += 1
+            if i % 2 == int(c): ans += 1
 
-        return min(anse, anso)
+        return min(ans, 1 - ans)
