0dc9b04936ba64d73dc11a71443877581d3932b0
Mindy Jen <jencmhep@gmail.com>
2024-09-17 14:17:54 -0400

    Update Minimum_Numbers_of_Operations_to_Make_Array_Empty.py

--- Medium/Arrays_Hashing/Minimum_Numbers_of_Operations_to_Make_Array_Empty.py
+++ Medium/Arrays_Hashing/Minimum_Numbers_of_Operations_to_Make_Array_Empty.py
@@ -10,3 +10,15 @@ class Solution:
             if v % 3 == 2 or v % 3 == 1: ans += 1
 
         return ans
+
+class Solution:
+    def minOperations(self, nums: List[int]) -> int:
+        
+        cp = Counter(nums)
+        ans = 0
+        
+        for k, v in cp.items():
+            if v == 1: return -1
+            ans += ceil(v / 3)
+
+        return ans
