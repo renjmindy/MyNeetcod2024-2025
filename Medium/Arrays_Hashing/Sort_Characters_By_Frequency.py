9d4a1556b3ef11bd09c2f3705fa17f5d545e7635
Mindy Jen <jencmhep@gmail.com>
2024-09-18 08:42:38 -0400

    Create Sort_Characters_By_Frequency.py

---
+++ Medium/Arrays_Hashing/Sort_Characters_By_Frequency.py
@@ -0,0 +1,12 @@
+class Solution:
+    def frequencySort(self, s: str) -> str:
+        
+        cp = Counter(s)
+        sp = sorted(cp.items(), key = lambda x:x[1], reverse=True)
+
+        ans = ''
+
+        for k, v in sp:
+            for i in range(v): ans += k
+
+        return ans
