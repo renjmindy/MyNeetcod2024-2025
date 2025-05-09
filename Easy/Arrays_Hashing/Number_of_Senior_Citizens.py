d9f1ad8849e273db341573722bf8e0ed3d2589d3
Mindy Jen <jencmhep@gmail.com>
2025-05-09 06:03:32 -0700

    Create Number_of_Senior_Citizens.py

---
+++ Easy/Arrays_Hashing/Number_of_Senior_Citizens.py
@@ -0,0 +1,9 @@
+class Solution:
+    def countSeniors(self, details: List[str]) -> int:
+        
+        ans = 0
+
+        for detail in details:
+            if int(detail[11:13]) > 60: ans += 1
+
+        return ans
