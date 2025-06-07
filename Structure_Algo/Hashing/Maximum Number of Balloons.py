3884b7760d18cbbfc456c412683e25aa15bfa2a8
Mindy Jen <jencmhep@gmail.com>
2025-06-07 01:13:55 -0700

    Create Maximum Number of Balloons.py

---
+++ Structure_Algo/Hashing/Maximum Number of Balloons.py
@@ -0,0 +1,5 @@
+class Solution:
+    def maxNumberOfBalloons(self, text: str) -> int:
+        
+        return min(Counter(text)['b'], Counter(text)['a'], Counter(text)['l'] // 2, 
+        Counter(text)['o'] // 2, Counter(text)['n'])
