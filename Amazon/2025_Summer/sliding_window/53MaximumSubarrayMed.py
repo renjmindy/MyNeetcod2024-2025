c6066239cf485db431619205d879de64e4575aff
Mindy Jen <jencmhep@gmail.com>
2025-07-31 06:15:42 -0700

    Update 53MaximumSubarrayMed.py

--- Amazon/2025_Summer/sliding_window/53MaximumSubarrayMed.py
+++ Amazon/2025_Summer/sliding_window/53MaximumSubarrayMed.py
@@ -1,1 +1,10 @@
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        
+        ans, pre = -math.inf, -math.inf
 
+        for r in range(len(nums)):
+            pre = max(pre + nums[r], nums[r])
+            ans = max(ans, pre)
+
+        return ans
