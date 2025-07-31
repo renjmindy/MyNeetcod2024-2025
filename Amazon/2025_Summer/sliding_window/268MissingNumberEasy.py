0217b46b4945bdd0cf81789e824dc3d5871ec253
Mindy Jen <jencmhep@gmail.com>
2025-07-31 06:20:04 -0700

    Update 268MissingNumberEasy.py

--- Amazon/2025_Summer/sliding_window/268MissingNumberEasy.py
+++ Amazon/2025_Summer/sliding_window/268MissingNumberEasy.py
@@ -1,1 +1,9 @@
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        
+        nums.sort()
 
+        for r in range(len(nums)):
+            if r != nums[r]: return r
+
+        return len(nums) 
