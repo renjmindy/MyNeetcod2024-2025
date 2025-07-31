5724e0caa04405e3cc6cbcc41a2f9bfb80e6bb2a
Mindy Jen <jencmhep@gmail.com>
2025-07-31 04:32:50 -0700

    Update 5LongestPalindromicSubstringMed.py

--- Amazon/2025_Summer/sliding_window/5LongestPalindromicSubstringMed.py
+++ Amazon/2025_Summer/sliding_window/5LongestPalindromicSubstringMed.py
@@ -1,1 +1,11 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        
+        ans = s[0]
 
+        for r in range(1, len(s)):
+            for l in range(r):
+                if len(ans) < len(s[l:r + 1]) and s[l:r + 1] == s[l:r + 1][::-1]:
+                    ans = s[l:r + 1]
+
+        return ans
