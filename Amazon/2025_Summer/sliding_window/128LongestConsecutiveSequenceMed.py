668567363c77ae9da4d48d7eab7850a639654502
Mindy Jen <jencmhep@gmail.com>
2025-07-31 05:26:20 -0700

    Update 128LongestConsecutiveSequenceMed.py

--- Amazon/2025_Summer/sliding_window/128LongestConsecutiveSequenceMed.py
+++ Amazon/2025_Summer/sliding_window/128LongestConsecutiveSequenceMed.py
@@ -1,1 +1,16 @@
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
 
+        if len(nums) == 0: return 0
+
+        mums = list(set(nums))
+        mums.sort()
+        l, ans = 0, 1 # ans = 1 b/c the length of the starting letter is one
+
+        for r in range(1, len(mums)):
+            if mums[r] - mums[r - 1] == 1: 
+                ans = max(ans, r - l + 1) # update consecutive sequence length
+            else: 
+                l = r # reset
+
+        return ans
