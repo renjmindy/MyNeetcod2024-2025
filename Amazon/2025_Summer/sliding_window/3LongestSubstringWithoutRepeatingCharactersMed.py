5482d356cd4384b438a73baf10b8c726922a559e
Mindy Jen <jencmhep@gmail.com>
2025-07-31 04:14:51 -0700

    Update 3LongestSubstringWithoutRepeatingCharactersMed.py

--- Amazon/2025_Summer/sliding_window/3LongestSubstringWithoutRepeatingCharactersMed.py
+++ Amazon/2025_Summer/sliding_window/3LongestSubstringWithoutRepeatingCharactersMed.py
@@ -1,1 +1,15 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        mp = defaultdict(int)
+        l, ans = 0, 0
 
+        for r in range(len(s)):
+            mp[s[r]] += 1
+
+            while mp[s[r]] == 2: # till the removel of repeating character
+                mp[s[l]] -= 1
+                l += 1
+
+            ans = max(ans, r - l + 1)
+
+        return ans
