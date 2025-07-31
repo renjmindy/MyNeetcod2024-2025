30f8e3ea4e22233f41d424f1f73da7f0c794a5cb
Mindy Jen <jencmhep@gmail.com>
2025-07-31 07:03:10 -0700

    Update 7ReverseIntegerMed.py

--- Amazon/2025_Summer/sliding_window/7ReverseIntegerMed.py
+++ Amazon/2025_Summer/sliding_window/7ReverseIntegerMed.py
@@ -1,1 +1,10 @@
+class Solution:
+    def reverse(self, x: int) -> int:
+        
+        sign = 1 if x > 0 else -1
 
+        x *= sign
+
+        n = int(''.join([c for c in str(x)][::-1]))
+
+        return sign * n if sign * n <= (2**31 - 1) and sign * n >= -2**31 else 0
