ee6802341e9304ddd1ba5b86900662dfbce1142e
Mindy Jen <jencmhep@gmail.com>
2025-07-31 04:37:40 -0700

    Update 9PalindromeNumberEasy.py

--- Amazon/2025_Summer/sliding_window/9PalindromeNumberEasy.py
+++ Amazon/2025_Summer/sliding_window/9PalindromeNumberEasy.py
@@ -1,1 +1,12 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        
+        sign = 1 if x > 0 else -1
 
+        if sign == -1 and x * sign > 0: return False
+
+        x *= sign
+
+        y = [c for c in str(x)][::-1]
+
+        return list(str(x)) == y
