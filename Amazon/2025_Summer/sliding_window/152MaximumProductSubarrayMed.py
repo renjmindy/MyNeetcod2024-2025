99b9488cab4dd79d7420ee62194144eaf72191eb
Mindy Jen <jencmhep@gmail.com>
2025-07-31 20:03:34 -0700

    Update 152MaximumProductSubarrayMed.py

--- Amazon/2025_Summer/sliding_window/152MaximumProductSubarrayMed.py
+++ Amazon/2025_Summer/sliding_window/152MaximumProductSubarrayMed.py
@@ -1,1 +1,15 @@
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
 
+        curmax, curmin, ans = nums[0], nums[0], nums[0]
+
+        for r in range(1, len(nums)):
+            if nums[r] < 0:
+                curmin, curmax = curmax, curmin
+
+            curmax = max(nums[r], nums[r] * curmax)
+            curmin = min(nums[r], nums[r] * curmin)
+
+            ans = max(ans, curmax)
+
+        return ans
