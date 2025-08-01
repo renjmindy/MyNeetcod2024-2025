a847a529be4328fb4908beefdf0053c1060baa3f
Mindy Jen <jencmhep@gmail.com>
2025-08-01 11:03:24 -0700

    Update 3019NumberofChangingKeysEasy.py

--- Amazon/2025_Summer/sliding_window/3019NumberofChangingKeysEasy.py
+++ Amazon/2025_Summer/sliding_window/3019NumberofChangingKeysEasy.py
@@ -1,1 +1,9 @@
+class Solution:
+    def countKeyChanges(self, s: str) -> int:
+        
+        ans = 0
 
+        for r in range(1, len(s)):
+            if s[r - 1].lower() != s[r].lower(): ans += 1
+
+        return ans
