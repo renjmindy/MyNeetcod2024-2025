4149824d7ae40250ae2d549057e81b7169a081c9
Mindy Jen <jencmhep@gmail.com>
2025-07-31 19:33:04 -0700

    Update 76MinimumWindowSubstringHard.py

--- Amazon/2025_Summer/sliding_window/76MinimumWindowSubstringHard.py
+++ Amazon/2025_Summer/sliding_window/76MinimumWindowSubstringHard.py
@@ -1,1 +1,20 @@
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        
+        mp = Counter(t)
+        ans = ''
+        l, cnt = 0, 0
 
+        for r in range(len(s)):
+            if mp[s[r]] > 0: cnt += 1
+            mp[s[r]] -= 1
+
+            if cnt == len(t):
+                while l < len(s) and mp[s[l]] < 0:
+                    mp[s[l]] += 1
+                    l += 1
+
+                if len(ans) > (r - l + 1) or len(ans) == 0:
+                    ans = s[l:r + 1]
+
+        return ans
