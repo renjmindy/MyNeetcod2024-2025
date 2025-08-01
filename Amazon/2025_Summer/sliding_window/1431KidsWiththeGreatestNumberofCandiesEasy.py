721e762dbd1046aa04ff5795c83a7ad707cb1128
Mindy Jen <jencmhep@gmail.com>
2025-08-01 10:49:11 -0700

    Update 1431KidsWiththeGreatestNumberofCandiesEasy.py

--- Amazon/2025_Summer/sliding_window/1431KidsWiththeGreatestNumberofCandiesEasy.py
+++ Amazon/2025_Summer/sliding_window/1431KidsWiththeGreatestNumberofCandiesEasy.py
@@ -1,1 +1,12 @@
+class Solution:
+    def kidsWithCandies(self, candies: List[int], extraCandies: int) -> List[bool]:
+        
+        max_candy = max(candies)
 
+        ans = list()
+
+        for candy in candies:
+            if extraCandies + candy >= max_candy: ans.append(True)
+            else: ans.append(False)
+
+        return ans
