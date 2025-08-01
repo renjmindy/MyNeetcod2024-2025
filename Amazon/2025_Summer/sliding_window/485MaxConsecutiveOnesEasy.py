b9e796ca849c86f523ce82af182ac98c6923f674
Mindy Jen <jencmhep@gmail.com>
2025-08-01 02:32:43 -0700

    Update 485MaxConsecutiveOnesEasy.py

--- Amazon/2025_Summer/sliding_window/485MaxConsecutiveOnesEasy.py
+++ Amazon/2025_Summer/sliding_window/485MaxConsecutiveOnesEasy.py
@@ -1,1 +1,14 @@
+class Solution:
+    def findMaxConsecutiveOnes(self, nums: List[int]) -> int:
+        
+        cnt, ans = 0, 0
 
+        for r in range(len(nums)):
+            if nums[r] == 1: cnt += 1
+            else:
+                ans = max(ans, cnt)
+                cnt = 0
+
+        ans = max(ans, cnt)
+
+        return ans
