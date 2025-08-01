9fa09f6e26da4b3e5978a151890433e29ccc2d26
Mindy Jen <jencmhep@gmail.com>
2025-07-31 18:45:00 -0700

    Update 66PlusOneEasy.py

--- Amazon/2025_Summer/sliding_window/66PlusOneEasy.py
+++ Amazon/2025_Summer/sliding_window/66PlusOneEasy.py
@@ -1,1 +1,8 @@
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        
+        n = int(''.join([str(digit) for digit in digits]))
 
+        n += 1
+
+        return [int(c) for c in str(n)]
