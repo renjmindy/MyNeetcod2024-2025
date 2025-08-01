f35d2966c0a8bfb66890f6d0c946ae09f5f78d26
Mindy Jen <jencmhep@gmail.com>
2025-07-31 22:00:45 -0700

    Update 214ShortestPalindromeHard.py

--- Amazon/2025_Summer/sliding_window/214ShortestPalindromeHard.py
+++ Amazon/2025_Summer/sliding_window/214ShortestPalindromeHard.py
@@ -1,1 +1,9 @@
+class Solution:
+    def shortestPalindrome(self, s: str) -> str:
 
+        if len(s) == 0: return ''
+
+        r = s[::-1]
+
+        for i in range(len(r)):
+            if s.startswith(r[i:]): return r[:i] + s
