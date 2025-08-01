f2a06ce5b33c4e58d4e80d54e47ce81a43467dd7
Mindy Jen <jencmhep@gmail.com>
2025-08-01 00:30:08 -0700

    Update 395LongestSubstringwithAtLeastKRepeatingCharactersMed.py

--- Amazon/2025_Summer/sliding_window/395LongestSubstringwithAtLeastKRepeatingCharactersMed.py
+++ Amazon/2025_Summer/sliding_window/395LongestSubstringwithAtLeastKRepeatingCharactersMed.py
@@ -1,1 +1,10 @@
+class Solution:
+    def longestSubstring(self, s: str, k: int) -> int:
+        
+        mp = Counter(s)
 
+        for key, v in mp.items():
+            if v < k:
+                return max([self.longestSubstring(c, k) for c in s.split(key)]) # split is to skip shorter length of character
+
+        return len(s)
