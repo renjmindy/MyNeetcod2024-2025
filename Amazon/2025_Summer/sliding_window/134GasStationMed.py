8a121af58358b17b29b40a90e3326f00a54ce15a
Mindy Jen <jencmhep@gmail.com>
2025-07-31 19:50:38 -0700

    Update 134GasStationMed.py

--- Amazon/2025_Summer/sliding_window/134GasStationMed.py
+++ Amazon/2025_Summer/sliding_window/134GasStationMed.py
@@ -1,1 +1,12 @@
+class Solution:
+    def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
+        
+        if sum(gas) < sum(cost): return -1
 
+        ans, dif = 0, 0
+
+        for r in range(len(gas)):
+            dif += gas[r] - cost[r]
+            if dif < 0: dif = 0; ans = r + 1
+
+        return ans
