aec3e80fc36b52930d61e9740f13b65e58494939
Mindy Jen <jencmhep@gmail.com>
2025-08-01 10:23:52 -0700

    Update 539MinimumTimeDifferenceMed.py

--- Amazon/2025_Summer/sliding_window/539MinimumTimeDifferenceMed.py
+++ Amazon/2025_Summer/sliding_window/539MinimumTimeDifferenceMed.py
@@ -1,1 +1,9 @@
+class Solution:
+    def findMinDifference(self, timePoints: List[str]) -> int:
+        
+        mins = [int(p[:2])*60 + int(p[3:]) for p in timePoints]
+        mins.sort()
 
+        mins.append(mins[0] + 1440)
+
+        return min(map(sub, mins[1:], mins))
