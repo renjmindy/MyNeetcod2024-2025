28377c1484775abdf05381cd0e6cc2055a216459
Mindy Jen <jencmhep@gmail.com>
2025-08-01 09:20:14 -0700

    Update 517SuperWashingMachinesHard.py

--- Amazon/2025_Summer/sliding_window/517SuperWashingMachinesHard.py
+++ Amazon/2025_Summer/sliding_window/517SuperWashingMachinesHard.py
@@ -1,1 +1,17 @@
+class Solution:
+    def findMinMoves(self, machines: List[int]) -> int:
+        
+        if sum(machines) % len(machines): return -1
 
+        cost = sum(machines) // len(machines)
+
+        ans, dif = 0, 0
+
+        for r in range(len(machines)):
+            dif += machines[r] - cost
+            # The maximum value of abs(dif) indicates the 
+            # maximum number of steps that needs to be 
+            # moved in a single move
+            ans = max(ans, abs(dif), machines[r] - cost)
+
+        return ans
