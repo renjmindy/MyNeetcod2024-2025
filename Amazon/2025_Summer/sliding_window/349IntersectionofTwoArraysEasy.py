042741969490a2e9eabbed17b63561bb57c30a8b
Mindy Jen <jencmhep@gmail.com>
2025-07-31 17:13:10 -0700

    Update 349IntersectionofTwoArraysEasy.py

--- Amazon/2025_Summer/sliding_window/349IntersectionofTwoArraysEasy.py
+++ Amazon/2025_Summer/sliding_window/349IntersectionofTwoArraysEasy.py
@@ -1,1 +1,11 @@
+class Solution:
+    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        
+        n1 = Counter(nums1)
+        n2 = Counter(nums2)
+        ans = list()
 
+        for k, v in n1.items():
+            if k in n2: ans.append(k)
+
+        return ans
