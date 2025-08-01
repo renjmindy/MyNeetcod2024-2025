76e3f66e335d47ee66c324ac776218ab5a9f887a
Mindy Jen <jencmhep@gmail.com>
2025-07-31 17:09:40 -0700

    Update 1268SearchSuggestionsSystemMed.py

--- Amazon/2025_Summer/sliding_window/1268SearchSuggestionsSystemMed.py
+++ Amazon/2025_Summer/sliding_window/1268SearchSuggestionsSystemMed.py
@@ -1,1 +1,11 @@
+class Solution:
+    def suggestedProducts(self, products: List[str], searchWord: str) -> List[List[str]]:
+        
+        ans = list()
+        products.sort()
 
+        for r in range(len(searchWord)):
+            products = [product for product in products if product.startswith(searchWord[:r + 1])]
+            ans.append(products[:3])
+
+        return ans
