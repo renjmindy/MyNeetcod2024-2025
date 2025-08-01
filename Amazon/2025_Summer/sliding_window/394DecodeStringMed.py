101471bc5ad5e4081cfb078dbb8e6d98d59e078d
Mindy Jen <jencmhep@gmail.com>
2025-08-01 00:08:01 -0700

    Update 394DecodeStringMed.py

--- Amazon/2025_Summer/sliding_window/394DecodeStringMed.py
+++ Amazon/2025_Summer/sliding_window/394DecodeStringMed.py
@@ -1,1 +1,22 @@
+class Solution:
+    def decodeString(self, s: str) -> str:
+        
+        num = 0
+        stack = list()
+        ans = ''
 
+        for c in s:
+            if c.isdigit(): num = num * 10 + int(c)
+            elif c == '[':
+                stack.append(num)
+                stack.append(ans) # push old string into stack
+                num = 0
+                ans = ''
+            elif c == ']':
+                pre = stack.pop() # pop old string
+                cnt = stack.pop()
+                ans = pre + ans * cnt # pop old string + new char
+            else:
+                ans += c
+
+        return ans
