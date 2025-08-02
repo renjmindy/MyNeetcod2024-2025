98f95fbc6f5f06e4e8c5cb9a636ebac7e89a7888
Mindy Jen <jencmhep@gmail.com>
2025-08-01 21:19:05 -0700

    Update 560SubarraySumEqualsKMed.py

--- Amazon/2025_Summer/dynamic_programming/560SubarraySumEqualsKMed.py
+++ Amazon/2025_Summer/dynamic_programming/560SubarraySumEqualsKMed.py
@@ -1,1 +1,14 @@
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        
+        mp = defaultdict(int)
+        mp[0] = 1
 
+        pre, ans = 0, 0
+
+        for r in range(len(nums)):
+            pre += nums[r]
+            if pre - k in mp: ans += mp[pre - k]
+            mp[pre] += 1
+
+        return ans
