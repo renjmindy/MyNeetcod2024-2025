627bb04ad70735816f48898140cdedbf2e123903
Mindy Jen <jencmhep@gmail.com>
2025-08-01 19:36:05 -0700

    Update 55JumpGameMed.py

--- Amazon/2025_Summer/dynamic_programming/55JumpGameMed.py
+++ Amazon/2025_Summer/dynamic_programming/55JumpGameMed.py
@@ -1,1 +1,13 @@
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
 
+        mp = [0] * len(nums)
+        mp[0] = nums[0]
+
+        for r in range(1, len(nums)):
+            if mp[r - 1] < r: return False
+            mp[r] = max(mp[r - 1], r + nums[r])
+
+        print(mp)
+
+        return mp[-1] >= (len(nums) - 1)
