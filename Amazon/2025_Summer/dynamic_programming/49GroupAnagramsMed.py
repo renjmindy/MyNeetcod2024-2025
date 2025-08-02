cb7dde22949af58acaa5d220747eea315dc101e6
Mindy Jen <jencmhep@gmail.com>
2025-08-01 17:54:53 -0700

    Update 49GroupAnagramsMed.py

--- Amazon/2025_Summer/dynamic_programming/49GroupAnagramsMed.py
+++ Amazon/2025_Summer/dynamic_programming/49GroupAnagramsMed.py
@@ -1,1 +1,10 @@
+from collections import Counter
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        
+        mp = defaultdict(list)
 
+        for str in strs:
+            mp[''.join(sorted(str))].append(str)
+
+        return list(mp.values())
