ef55744d8d12ea61cad6201e7ac5ec9839946e9e
Mindy Jen <jencmhep@gmail.com>
2025-08-02 02:01:02 -0700

    Update 322CoinChangeMed.py

--- Amazon/2025_Summer/dynamic_programming/322CoinChangeMed.py
+++ Amazon/2025_Summer/dynamic_programming/322CoinChangeMed.py
@@ -1,1 +1,12 @@
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        
+        mp = [math.inf] * (amount + 1)
+        mp[0] = 0
 
+        for r in range(1, amount + 1):
+            for coin in coins:
+                if r >= coin:
+                    mp[r] = min(mp[r], 1 + mp[r - coin])
+
+        return mp[amount] if mp[amount] < math.inf else -1
