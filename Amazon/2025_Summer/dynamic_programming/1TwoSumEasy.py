699959cf933a8d78fa9d7619262b1648bc578743
Mindy Jen <jencmhep@gmail.com>
2025-08-01 17:49:01 -0700

    Update 1TwoSumEasy.py

--- Amazon/2025_Summer/dynamic_programming/1TwoSumEasy.py
+++ Amazon/2025_Summer/dynamic_programming/1TwoSumEasy.py
@@ -1,1 +1,13 @@
-
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        
+        mp = defaultdict(int)
+        ans = list()
+        
+        for r in range(len(nums)):
+            if target - nums[r] in mp:
+                ans.append(mp[target - nums[r]])
+                ans.append(r)
+            mp[nums[r]] = r
+            
+        return ans
