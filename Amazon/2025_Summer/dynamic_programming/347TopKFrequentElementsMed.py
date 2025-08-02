17edbf0cb25e9a6e6830a09a7e0824afc527de6d
Mindy Jen <jencmhep@gmail.com>
2025-08-01 20:47:52 -0700

    Update 347TopKFrequentElementsMed.py

--- Amazon/2025_Summer/dynamic_programming/347TopKFrequentElementsMed.py
+++ Amazon/2025_Summer/dynamic_programming/347TopKFrequentElementsMed.py
@@ -1,1 +1,10 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        
+        mp = Counter(nums)
 
+        ans = list()
+
+        mp_sorted = sorted(mp.items(), key = lambda x:x[1], reverse=True)[:k]
+
+        return list(k for k, v in mp_sorted)
