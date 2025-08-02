deefcf29523283fa82776b14c1ec0e7f702af474
Mindy Jen <jencmhep@gmail.com>
2025-08-02 00:32:43 -0700

    Update 139WordBreakMed.py

--- Amazon/2025_Summer/dynamic_programming/139WordBreakMed.py
+++ Amazon/2025_Summer/dynamic_programming/139WordBreakMed.py
@@ -1,1 +1,15 @@
 
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        
+        wordset = set(wordDict)
+
+        mp = [False] * (len(s) + 1)
+
+        mp[0] = True
+
+        for r in range(1, len(s) + 1):
+            for l in range(r):
+                if mp[l] == True and s[l:r] in wordset: mp[r] = True; break
+
+        return mp[-1]
