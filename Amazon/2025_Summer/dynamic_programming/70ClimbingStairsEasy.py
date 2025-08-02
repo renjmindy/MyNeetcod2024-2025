9826704c16b4a1529d684284b9581bb0ef0c6073
Mindy Jen <jencmhep@gmail.com>
2025-08-01 21:30:24 -0700

    Update 70ClimbingStairsEasy.py

--- Amazon/2025_Summer/dynamic_programming/70ClimbingStairsEasy.py
+++ Amazon/2025_Summer/dynamic_programming/70ClimbingStairsEasy.py
@@ -1,1 +1,14 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
 
+        if n <= 2: return n
+
+        mp = [0] * (n + 1)
+
+        mp[1] = 1
+        mp[2] = 2
+
+        for r in range(3, n + 1):
+            mp[r] = mp[r - 1] + mp[r - 2]
+
+        return mp[n]
