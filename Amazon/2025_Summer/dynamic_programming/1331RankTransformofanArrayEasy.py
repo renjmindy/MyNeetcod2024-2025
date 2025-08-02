ffeaa6106dd2a991673cda098d08a79ad3611177
Mindy Jen <jencmhep@gmail.com>
2025-08-02 04:47:20 -0700

    Update 1331RankTransformofanArrayEasy.py

--- Amazon/2025_Summer/dynamic_programming/1331RankTransformofanArrayEasy.py
+++ Amazon/2025_Summer/dynamic_programming/1331RankTransformofanArrayEasy.py
@@ -1,1 +1,12 @@
+class Solution:
+    def arrayRankTransform(self, arr: List[int]) -> List[int]:
+        
+        b = list(set(arr))
+        b.sort()
 
+        mp = defaultdict(int)
+
+        for i, j in enumerate(b):
+            mp[j] = i + 1
+
+        return [mp[a] for a in arr]
