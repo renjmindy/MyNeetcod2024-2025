1aaeb4f2182129c3b44781eb896fa55fc6af8401
Mindy Jen <jencmhep@gmail.com>
2025-08-02 05:57:44 -0700

    Update 169MajorityElementEasy.py

--- Amazon/2025_Summer/dynamic_programming/169MajorityElementEasy.py
+++ Amazon/2025_Summer/dynamic_programming/169MajorityElementEasy.py
@@ -1,1 +1,8 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        
+        mp = Counter(nums)
 
+        mp_sorted = sorted(mp.items(), key = lambda x:x[1], reverse=True)[:1]
+
+        return mp_sorted[0][0]
