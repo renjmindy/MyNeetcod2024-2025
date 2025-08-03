149497ebcc08d1228fbd06794236d9a93ac2e79b
Mindy Jen <jencmhep@gmail.com>
2025-08-03 02:00:12 -0700

    Update 198HouseRobberMed.py

--- Amazon/2025_Summer/dynamic_programming/198HouseRobberMed.py
+++ Amazon/2025_Summer/dynamic_programming/198HouseRobberMed.py
@@ -1,1 +1,14 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        
+        if len(nums) <= 2: return max(nums)
 
+        mp = [0] * len(nums)
+
+        mp[0] = nums[0]
+        mp[1] = max(nums[0], nums[1])
+
+        for r in range(2, len(nums)):
+            mp[r] = max(mp[r - 1], nums[r] + mp[r - 2])
+
+        return mp[-1]
