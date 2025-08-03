7ff821529a753f394b4a7fa1faad452b69079984
Mindy Jen <jencmhep@gmail.com>
2025-08-02 18:05:56 -0700

    Update 1010PairsofSongsWithTotalDurationsDivisibleby60Med.py

--- Amazon/2025_Summer/dynamic_programming/1010PairsofSongsWithTotalDurationsDivisibleby60Med.py
+++ Amazon/2025_Summer/dynamic_programming/1010PairsofSongsWithTotalDurationsDivisibleby60Med.py
@@ -2,11 +2,10 @@ class Solution:
     def numPairsDivisibleBy60(self, time: List[int]) -> int:
         
         mp = defaultdict(int)
-        mp[60] = 1
         ans = 0
 
         for r in range(len(time)):
-            if 60 - (time[r] % 60) in mp: ans += mp[(60 - (time[r] % 60)) % 60]
+            if (60 - (time[r] % 60)) % 60 in mp: ans += mp[(60 - (time[r] % 60)) % 60]
             mp[time[r] % 60] += 1 
 
         return ans
