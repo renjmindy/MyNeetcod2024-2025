e2c8106a705d1379232443bd4f06a59b11229c95
Mindy Jen <jencmhep@gmail.com>
2025-08-02 19:21:50 -0700

    Update 1497CheckIfArrayPairsAreDivisiblebykMed.py

--- Amazon/2025_Summer/dynamic_programming/1497CheckIfArrayPairsAreDivisiblebykMed.py
+++ Amazon/2025_Summer/dynamic_programming/1497CheckIfArrayPairsAreDivisiblebykMed.py
@@ -1,1 +1,15 @@
+class Solution:
+    def canArrange(self, arr: List[int], k: int) -> bool:
+        
+        mp = defaultdict(int)
+        
 
+        for r in range(len(arr)):
+            mp[arr[r] % k] += 1
+
+        if mp[0] % 2: return False
+
+        for key, v in mp.items():
+            if mp[key] != mp[(k - key) % k]: return False
+
+        return True
