4082572f2577aebc19edfe8cd605c6c3bb176e1f
Mindy Jen <jencmhep@gmail.com>
2025-08-03 06:49:03 -0700

    Update 2491DividePlayersIntoTeamsofEqualSkillMed.py

--- Amazon/2025_Summer/dynamic_programming/2491DividePlayersIntoTeamsofEqualSkillMed.py
+++ Amazon/2025_Summer/dynamic_programming/2491DividePlayersIntoTeamsofEqualSkillMed.py
@@ -1,1 +1,11 @@
+class Solution:
+    def dividePlayers(self, skill: List[int]) -> int:
+        skill.sort()
 
+        ans = 0
+
+        for r in range(len(skill) // 2):
+            if skill[r] + skill[len(skill) - r - 1] != skill[0] + skill[-1]: return -1
+            ans += skill[r] * skill[len(skill) - r - 1]
+
+        return ans
