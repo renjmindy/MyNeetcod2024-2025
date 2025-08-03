509fd17097295fbbb30b3bb0d1e774152037847b
Mindy Jen <jencmhep@gmail.com>
2025-08-02 20:26:35 -0700

    Update 135CandyHard.py

--- Amazon/2025_Summer/dynamic_programming/135CandyHard.py
+++ Amazon/2025_Summer/dynamic_programming/135CandyHard.py
@@ -1,1 +1,18 @@
+class Solution:
+    def candy(self, ratings: List[int]) -> int:
+        
+        mp = [1] * len(ratings)
 
+        ans = 0
+
+        for l in range(1, len(ratings)):
+            if ratings[l - 1] < ratings[l] and mp[l - 1] >= mp[l]: mp[l] = 1 + mp[l - 1]
+
+        print(mp)
+
+        for r in range(len(ratings) - 2, -1, -1):
+            if ratings[r + 1] < ratings[r] and mp[r + 1] >= mp[r]: mp[r] = 1 + mp[r + 1]
+
+        print(mp)
+
+        return sum(mp)
