400d601ffa8a2e7aa00afe02c9b54bd12331ffc3
Mindy Jen <jencmhep@gmail.com>
2025-08-03 03:04:25 -0700

    Update 442FindAllDuplicatesinanArrayMed.py

--- Amazon/2025_Summer/dynamic_programming/442FindAllDuplicatesinanArrayMed.py
+++ Amazon/2025_Summer/dynamic_programming/442FindAllDuplicatesinanArrayMed.py
@@ -1,1 +1,13 @@
+class Solution:
+    def findDuplicates(self, nums: List[int]) -> List[int]:
+        
+        mp = defaultdict(int)
 
+        ans = list()
+
+        for num in nums: mp[num] += 1
+
+        for k, v in mp.items():
+            if v > 1: ans.append(k)
+
+        return ans
