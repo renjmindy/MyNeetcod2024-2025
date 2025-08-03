720d1728aa3b7ceba8972de0aae0456321eaec4d
Mindy Jen <jencmhep@gmail.com>
2025-08-03 04:50:57 -0700

    Update 1590MakeSumDivisiblebyPMed.py

--- Amazon/2025_Summer/dynamic_programming/1590MakeSumDivisiblebyPMed.py
+++ Amazon/2025_Summer/dynamic_programming/1590MakeSumDivisiblebyPMed.py
@@ -1,1 +1,20 @@
+ total = sum(nums)
+        rem = total % p
 
+        if rem == 0: return 0
+
+        mp = defaultdict(int)
+        mp[0] = -1
+        pre = 0
+        ans = len(nums)
+
+        for r in range(len(nums)):
+            pre += nums[r]
+            mod = ((pre % p) - rem + p) % p
+
+            if mod in mp:
+                ans = min(ans, r - mp[mod])
+
+            mp[pre % p] = r
+
+        return ans if ans < len(nums) else -1
