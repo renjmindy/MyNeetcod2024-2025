bf7be4ec1cbfca70e5f95198bbe67de36f3dfa58
Mindy Jen <jencmhep@gmail.com>
2025-08-02 20:58:03 -0700

    Update 155MinStackMed.py

--- Amazon/2025_Summer/dynamic_programming/155MinStackMed.py
+++ Amazon/2025_Summer/dynamic_programming/155MinStackMed.py
@@ -1,1 +1,24 @@
+class MinStack:
 
+    def __init__(self):
+        self.mp = list()
+
+    def push(self, val: int) -> None:
+        self.mp.append(val)
+
+    def pop(self) -> None:
+        if len(self.mp): self.mp.pop()
+
+    def top(self) -> int:
+        if len(self.mp): return self.mp[-1]
+
+    def getMin(self) -> int:
+        if len(self.mp): return min(self.mp)
+
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(val)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
