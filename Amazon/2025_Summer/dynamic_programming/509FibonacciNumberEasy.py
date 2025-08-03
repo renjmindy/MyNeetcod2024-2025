ca12a137ee80f4d38271805e3c96b6b4113daab7
Mindy Jen <jencmhep@gmail.com>
2025-08-03 03:27:41 -0700

    Update 509FibonacciNumberEasy.py

--- Amazon/2025_Summer/dynamic_programming/509FibonacciNumberEasy.py
+++ Amazon/2025_Summer/dynamic_programming/509FibonacciNumberEasy.py
@@ -1,1 +1,12 @@
+class Solution:
+    def fib(self, n: int) -> int:
+        if n <= 1: return n
 
+        mp = [0] * (n + 1)
+        mp[0] = 0
+        mp[1] = 1
+
+        for r in range(2, n + 1):
+            mp[r] = mp[r - 1] + mp[r - 2]
+
+        return mp[n]
