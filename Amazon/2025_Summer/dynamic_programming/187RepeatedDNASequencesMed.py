cfa8722da16ca1e7771ab90defa447ce1b7f88d4
Mindy Jen <jencmhep@gmail.com>
2025-08-02 21:36:31 -0700

    Update 187RepeatedDNASequencesMed.py

--- Amazon/2025_Summer/dynamic_programming/187RepeatedDNASequencesMed.py
+++ Amazon/2025_Summer/dynamic_programming/187RepeatedDNASequencesMed.py
@@ -1,1 +1,9 @@
+class Solution:
+    def findRepeatedDnaSequences(self, s: str) -> List[str]:
+        
+        mp = defaultdict(int)
 
+        for r in range(len(s) - 9):
+            mp[s[r : r + 10]] += 1
+
+        return [k for k, v in mp.items() if v > 1]
