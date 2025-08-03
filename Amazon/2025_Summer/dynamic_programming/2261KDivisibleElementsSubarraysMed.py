7287b2e7e7959b81d6c306eff3f1b9e6b4a95752
Mindy Jen <jencmhep@gmail.com>
2025-08-03 06:31:15 -0700

    Update 2261KDivisibleElementsSubarraysMed.py

--- Amazon/2025_Summer/dynamic_programming/2261KDivisibleElementsSubarraysMed.py
+++ Amazon/2025_Summer/dynamic_programming/2261KDivisibleElementsSubarraysMed.py
@@ -1,1 +1,20 @@
+class Solution:
+    def countDistinct(self, nums: List[int], k: int, p: int) -> int:
+        
+        l, cnt, mp = 0, 0, set()
 
+        for r in range(len(nums)):
+            if nums[r] % p == 0: cnt += 1
+
+            while cnt > k:
+                if nums[l] % p == 0: cnt -= 1
+                l += 1
+
+            ans = nums[l:r + 1]
+            #print(ans)
+
+            for i in range(len(ans)):
+                #print(tuple(ans[i:]))
+                mp.add(tuple(ans[i:]))
+
+        return len(mp)
