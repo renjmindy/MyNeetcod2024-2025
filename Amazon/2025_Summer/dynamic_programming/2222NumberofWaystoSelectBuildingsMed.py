a7e644efb155cd7ee03feb6d46b5e480408aecbc
Mindy Jen <jencmhep@gmail.com>
2025-08-03 05:26:46 -0700

    Update 2222NumberofWaystoSelectBuildingsMed.py

--- Amazon/2025_Summer/dynamic_programming/2222NumberofWaystoSelectBuildingsMed.py
+++ Amazon/2025_Summer/dynamic_programming/2222NumberofWaystoSelectBuildingsMed.py
@@ -1,1 +1,17 @@
+class Solution:
+    def numberOfWays(self, s: str) -> int:
+        
+        ones = Counter(s)['1']
+        zeros = Counter(s)['0']
 
+        ans, cnt_ones, cnt_zeros = 0, 0, 0
+
+        for r in range(len(s)):
+            if s[r] == '0':
+                ans += cnt_ones * (ones - cnt_ones)
+                cnt_zeros += 1
+            if s[r] == '1':
+                ans += cnt_zeros * (zeros - cnt_zeros)
+                cnt_ones += 1
+
+        return ans
