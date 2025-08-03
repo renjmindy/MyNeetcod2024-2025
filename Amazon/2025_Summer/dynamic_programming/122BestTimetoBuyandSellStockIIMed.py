32da5a130f9a61229c0b587d2397f5f53c732547
Mindy Jen <jencmhep@gmail.com>
2025-08-02 19:43:24 -0700

    Update 122BestTimetoBuyandSellStockIIMed.py

--- Amazon/2025_Summer/dynamic_programming/122BestTimetoBuyandSellStockIIMed.py
+++ Amazon/2025_Summer/dynamic_programming/122BestTimetoBuyandSellStockIIMed.py
@@ -1,1 +1,14 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        
+        l, ans = 0, 0
 
+        for r in range(1, len(prices)):
+            buy = prices[l]
+            sell = prices[r]
+
+            if sell > buy: ans += sell - buy
+
+            l += 1
+
+        return ans
