12436649f99b2ea65a49f8e1935015f9f4eb77cd
Mindy Jen <jencmhep@gmail.com>
2025-08-03 02:39:44 -0700

    Update 287FindtheDuplicateNumberMed.py

--- Amazon/2025_Summer/dynamic_programming/287FindtheDuplicateNumberMed.py
+++ Amazon/2025_Summer/dynamic_programming/287FindtheDuplicateNumberMed.py
@@ -1,1 +1,9 @@
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        
+        mp = Counter(nums)
 
+        for k, v in mp.items():
+            if v > 1: return k
+
+        return -1
