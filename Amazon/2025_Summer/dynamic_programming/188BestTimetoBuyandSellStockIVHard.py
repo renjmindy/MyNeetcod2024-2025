4ce61dc93eac486bf2c637f2b3c3213fc19f28f6
Mindy Jen <jencmhep@gmail.com>
2025-08-02 22:40:50 -0700

    Update 188BestTimetoBuyandSellStockIVHard.py

--- Amazon/2025_Summer/dynamic_programming/188BestTimetoBuyandSellStockIVHard.py
+++ Amazon/2025_Summer/dynamic_programming/188BestTimetoBuyandSellStockIVHard.py
@@ -1,1 +1,16 @@
+class Solution:
+    def maxProfit(self, k: int, prices: List[int]) -> int:
 
+        buy_mp, sell_mp = [0] * len(prices), [0] * len(prices)
+
+        buy_mp[0] = -prices[0]
+        sell_mp[0] = 0
+
+        for i in range(k):
+            buy_mp[0] = -prices[0]
+            for r in range(1, len(prices)):
+                buy_mp[r] = max(buy_mp[r - 1], sell_mp[r] - prices[r])
+                sell_mp[r] = max(sell_mp[r - 1], buy_mp[r] + prices[r])
+
+        
+        return sell_mp[-1]
