92b2fd81c9fda480710219b7873f45af7a1e204e
Mindy Jen <jencmhep@gmail.com>
2025-08-03 22:57:25 -0700

    Update 921MinimumAddtoMakeParenthesesValidMed.py

--- Amazon/2025_Summer/two_pointer/921MinimumAddtoMakeParenthesesValidMed.py
+++ Amazon/2025_Summer/two_pointer/921MinimumAddtoMakeParenthesesValidMed.py
@@ -1,1 +1,10 @@
+class Solution:
+    def minAddToMakeValid(self, s: str) -> int:
+        
+        stack = list()
 
+        for r in range(len(s)):
+            if s[r] == ')' and stack and stack[-1] == '(': stack.pop()
+            else: stack.append(s[r])
+
+        return len(stack)
