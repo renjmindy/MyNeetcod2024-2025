8631217890f796e7eeb5732dae0cc10671740073
Mindy Jen <jencmhep@gmail.com>
2025-08-04 01:11:39 -0700

    Update 153SumMed.py

--- Amazon/2025_Summer/two_pointer/153SumMed.py
+++ Amazon/2025_Summer/two_pointer/153SumMed.py
@@ -1,1 +1,23 @@
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        
+        nums.sort()
 
+        ans = list()
+
+        for i in range(len(nums) - 2):
+            if i > 0 and nums[i - 1] == nums[i]: continue
+            l, r = i + 1, len(nums) - 1
+
+            while l < r:
+                tot = nums[i] + nums[l] + nums[r]
+                if tot == 0:
+                    ans.append([nums[i], nums[l], nums[r]])
+                    l += 1
+                    r -= 1
+                    while l < r and nums[l] == nums[l - 1]: l += 1
+                    while l < r and nums[r] == nums[r + 1]: r -= 1
+                elif tot > 0: r -= 1
+                else: l += 1
+
+        return ans
