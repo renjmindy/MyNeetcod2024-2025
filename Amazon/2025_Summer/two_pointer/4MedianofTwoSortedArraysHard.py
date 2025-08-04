8bf2f8b7cd312ecb21c4b3864e911d8ba92d909b
Mindy Jen <jencmhep@gmail.com>
2025-08-03 20:20:41 -0700

    Update 4MedianofTwoSortedArraysHard.py

--- Amazon/2025_Summer/two_pointer/4MedianofTwoSortedArraysHard.py
+++ Amazon/2025_Summer/two_pointer/4MedianofTwoSortedArraysHard.py
@@ -1,1 +1,7 @@
+class Solution:
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        
+        num = nums1 + nums2
+        num.sort()
 
+        return num[len(num) // 2] if len(num) % 2 else ((num[len(num) // 2 - 1] + num[len(num) // 2])) / 2
