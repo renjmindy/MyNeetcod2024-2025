782c03b9b033da70adb6a5c55ead25082b9a13d8
Mindy Jen <jencmhep@gmail.com>
2025-08-03 19:45:59 -0700

    Update 1768MergeStringsAlternatelyEasy.py

--- Amazon/2025_Summer/two_pointer/1768MergeStringsAlternatelyEasy.py
+++ Amazon/2025_Summer/two_pointer/1768MergeStringsAlternatelyEasy.py
@@ -1,1 +1,12 @@
+class Solution:
+    def mergeAlternately(self, word1: str, word2: str) -> str:
+        
+        l1, l2 = 0, 0
 
+        ans = list()
+
+        while l1 < len(word1) or l2 < len(word2):
+            if l1 < len(word1): ans.append(word1[l1]); l1 += 1
+            if l2 < len(word2): ans.append(word2[l2]); l2 += 1
+
+        return ''.join(ans)
