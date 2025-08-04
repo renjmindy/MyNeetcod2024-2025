f3c2c0622ad6e958a527fe8df0d1b0c414f0d8e8
Mindy Jen <jencmhep@gmail.com>
2025-08-03 17:20:57 -0700

    Update 875KokoEatingBananasMed.py

--- Amazon/2025_Summer/two_pointer/875KokoEatingBananasMed.py
+++ Amazon/2025_Summer/two_pointer/875KokoEatingBananasMed.py
@@ -1,1 +1,14 @@
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        
+        l, r = 1, max(piles) 
 
+        while l <= r:
+            pivot = l + (r - l) // 2 # eating speed (bananas / per hour)
+
+            time = sum([math.ceil(pile * 1.0 / pivot) for pile in piles])
+
+            if time > h: l = pivot + 1
+            else: r = pivot - 1
+
+        return max(l, r)
