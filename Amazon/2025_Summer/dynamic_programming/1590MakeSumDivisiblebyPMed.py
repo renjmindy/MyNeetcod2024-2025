8e4f889854f607cb2c02a923c27097976ed7da7c
Mindy Jen <jencmhep@gmail.com>
2025-08-03 17:38:11 -0700

    Update 1590MakeSumDivisiblebyPMed.py

--- Amazon/2025_Summer/dynamic_programming/1590MakeSumDivisiblebyPMed.py
+++ Amazon/2025_Summer/dynamic_programming/1590MakeSumDivisiblebyPMed.py
@@ -1,4 +1,28 @@
- total = sum(nums)
+class Solution:
+    def minSubarray(self, nums: List[int], p: int) -> int:
+        tot = sum(nums)
+        rem = tot % p
+
+        if rem == 0: return 0
+
+        ans, pre, mp = len(nums), 0, defaultdict(int) # trick: upper bound of ans is the overall length of array
+        mp[0] = -1
+
+        for r in range(len(nums)):
+            pre += nums[r]
+
+            ans_rem = (pre % p - rem) % p
+
+            if ans_rem in mp: ans = min(ans, r - mp[ans_rem])
+
+            mp[pre % p] = r
+
+        return ans if ans < len(nums) else -1 
+
+
+-----------------------------------------------------------------------------------------------------------------------------
+
+total = sum(nums)
         rem = total % p
 
         if rem == 0: return 0
