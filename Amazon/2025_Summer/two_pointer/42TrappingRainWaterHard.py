c19ad173fe3293407b814dbe2dc1af66a1825ed0
Mindy Jen <jencmhep@gmail.com>
2025-08-03 19:24:19 -0700

    Update 42TrappingRainWaterHard.py

--- Amazon/2025_Summer/two_pointer/42TrappingRainWaterHard.py
+++ Amazon/2025_Summer/two_pointer/42TrappingRainWaterHard.py
@@ -1,1 +1,16 @@
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        
+        ans = 0
+        stack = list()
 
+        for r in range(len(height)):
+            while stack and height[stack[-1]] < height[r]:
+                mid = stack.pop()
+                if not stack: break
+                l = stack[-1]
+                minH = min(height[r] - height[mid], height[l] - height[mid])
+                ans += minH * (r - l - 1)
+            stack.append(r)
+
+        return ans
