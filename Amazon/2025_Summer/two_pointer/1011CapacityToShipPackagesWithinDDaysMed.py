5f57c1473dc26c4d8e07003099f28c8413dde4c6
Mindy Jen <jencmhep@gmail.com>
2025-08-05 03:53:52 -0700

    Update 1011CapacityToShipPackagesWithinDDaysMed.py

--- Amazon/2025_Summer/two_pointer/1011CapacityToShipPackagesWithinDDaysMed.py
+++ Amazon/2025_Summer/two_pointer/1011CapacityToShipPackagesWithinDDaysMed.py
@@ -1,1 +1,17 @@
+class Solution:
+    def shipWithinDays(self, weights: List[int], days: int) -> int:
+        
+        l, r = max(weights), sum(weights)
 
+        while l <= r:
+            pivot = l + (r - l) // 2
+            pre, cnt = 0, 1
+
+            for weight in weights:
+                if pre + weight <= pivot: pre += weight
+                else: pre = weight; cnt += 1
+
+            if cnt <= days: r = pivot - 1
+            else: l = pivot + 1
+
+        return l
