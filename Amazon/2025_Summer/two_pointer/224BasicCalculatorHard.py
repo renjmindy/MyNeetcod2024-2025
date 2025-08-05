ea8a492afd265b88e49f24d098c053e6dd3f2a95
Mindy Jen <jencmhep@gmail.com>
2025-08-04 23:14:06 -0700

    Update 224BasicCalculatorHard.py

--- Amazon/2025_Summer/two_pointer/224BasicCalculatorHard.py
+++ Amazon/2025_Summer/two_pointer/224BasicCalculatorHard.py
@@ -1,1 +1,23 @@
+class Solution:
+    def calculate(self, s: str) -> int:
+        
+        stack = list()
 
+        sign, num = 1, 0
+
+        ans = 0
+
+        stack.append(sign)
+
+        for c in s:
+            if c.isdigit(): num = num * 10 + int(c)
+            elif c == '(':
+                stack.append(sign)
+            elif c == ')':
+                stack.pop()
+            elif c in ('+', '-'):
+                ans += sign * num
+                sign = (1 if c == '+' else -1) * stack[-1]
+                num = 0
+
+        return ans + sign * num
