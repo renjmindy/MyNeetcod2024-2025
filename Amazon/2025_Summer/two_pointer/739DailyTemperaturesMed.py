0fa08e40bd9d773801d12c21c876b19b1f1744ca
Mindy Jen <jencmhep@gmail.com>
2025-08-05 00:12:12 -0700

    Update 739DailyTemperaturesMed.py

--- Amazon/2025_Summer/two_pointer/739DailyTemperaturesMed.py
+++ Amazon/2025_Summer/two_pointer/739DailyTemperaturesMed.py
@@ -1,1 +1,14 @@
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        
+        stack = list()
 
+        ans = [0] * len(temperatures)
+
+        for r in range(len(temperatures)):
+            while stack and temperatures[stack[-1]] < temperatures[r]:
+                idx = stack.pop()
+                ans[idx] = (r - idx)
+            stack.append(r)
+
+        return ans
