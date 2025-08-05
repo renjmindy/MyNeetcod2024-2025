2976291b877a325fc33ee489371914c2b479b1c3
Mindy Jen <jencmhep@gmail.com>
2025-08-05 00:47:57 -0700

    Update 528RandomPickwithWeightMed.py

--- Amazon/2025_Summer/two_pointer/528RandomPickwithWeightMed.py
+++ Amazon/2025_Summer/two_pointer/528RandomPickwithWeightMed.py
@@ -1,1 +1,20 @@
+class Solution:
 
+    def __init__(self, w: List[int]):
+        pre = 0
+        self.mp = list()
+
+        for r in range(len(w)):
+            pre += w[r]
+            self.mp.append(pre)
+
+        self.pre = pre
+
+    def pickIndex(self) -> int:
+        r = random.randint(1, self.pre)
+        return bisect.bisect_left(self.mp, r)
+
+
+# Your Solution object will be instantiated and called as such:
+# obj = Solution(w)
+# param_1 = obj.pickIndex()
