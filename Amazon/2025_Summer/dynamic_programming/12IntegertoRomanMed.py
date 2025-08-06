092abce89c1bd2a2ecc5653a14895969d0ebc588
Mindy Jen <jencmhep@gmail.com>
2025-08-06 07:33:24 -0700

    Create 12IntegertoRomanMed.py

---
+++ Amazon/2025_Summer/dynamic_programming/12IntegertoRomanMed.py
@@ -0,0 +1,9 @@
+class Solution:
+    def intToRoman(self, num: int) -> str:
+        
+        ones = ['', 'I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX']
+        tens = ['', 'X', 'XX', 'XXX', 'XL', 'L', 'LX', 'LXX', 'LXXX', 'XC']
+        hrs = ['', 'C', 'CC', 'CCC', 'CD', 'D', 'DC', 'DCC', 'DCCC', 'CM']
+        ths = ['', 'M', 'MM', 'MMM']
+
+        return ths[num // 1000] + hrs[(num % 1000) // 100] + tens[(num % 100) // 10] + ones[num % 10]
