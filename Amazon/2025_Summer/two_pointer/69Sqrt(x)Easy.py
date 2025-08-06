5b4a16bf2b35555137a86851a75b85e136b26bce
Mindy Jen <jencmhep@gmail.com>
2025-08-05 18:25:27 -0700

    Update 69Sqrt(x)Easy.py

--- Amazon/2025_Summer/two_pointer/69Sqrt(x)Easy.py
+++ Amazon/2025_Summer/two_pointer/69Sqrt(x)Easy.py
@@ -1,1 +1,14 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        
+        if x <= 1: return x
 
+        l, r = 1, x // 2
+
+        while l <= r:
+            pivot = l + (r - l) // 2
+            if pivot ** 2 == x: return pivot
+            elif pivot ** 2 < x: l = pivot + 1
+            else: r = pivot - 1
+
+        return r
