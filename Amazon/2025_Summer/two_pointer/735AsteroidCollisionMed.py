1f601043806cf39d6fb60b38a0ddb471d3e3ab18
Mindy Jen <jencmhep@gmail.com>
2025-08-05 22:55:57 -0700

    Update 735AsteroidCollisionMed.py

--- Amazon/2025_Summer/two_pointer/735AsteroidCollisionMed.py
+++ Amazon/2025_Summer/two_pointer/735AsteroidCollisionMed.py
@@ -1,1 +1,15 @@
+class Solution:
+    def asteroidCollision(self, asteroids: List[int]) -> List[int]:
+        
+        stack = list()
 
+        for r in range(len(asteroids)):
+            col = False
+            while stack and stack[-1] > 0 and asteroids[r] < 0:
+                if stack[-1] > abs(asteroids[r]): col = True; break
+                if stack[-1] == abs(asteroids[r]): col = True; stack.pop(); break
+                if stack[-1] < abs(asteroids[r]): stack.pop()
+            
+            if not col: stack.append(asteroids[r])
+
+        return stack
