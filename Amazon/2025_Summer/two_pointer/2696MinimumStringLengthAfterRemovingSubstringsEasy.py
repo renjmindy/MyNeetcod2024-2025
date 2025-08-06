382e8ce79b3244ef508749cd2410494b948155a4
Mindy Jen <jencmhep@gmail.com>
2025-08-05 23:14:11 -0700

    Update 2696MinimumStringLengthAfterRemovingSubstringsEasy.py

--- Amazon/2025_Summer/two_pointer/2696MinimumStringLengthAfterRemovingSubstringsEasy.py
+++ Amazon/2025_Summer/two_pointer/2696MinimumStringLengthAfterRemovingSubstringsEasy.py
@@ -1,1 +1,17 @@
+class Solution:
+    def minLength(self, s: str) -> int:
+        
+        stack = list()
 
+        ans = 0
+
+        for c in s:
+            found = False
+            while not found and stack and ((stack[-1] == 'A' and c == 'B') or (stack[-1] == 'C' and c == 'D')):
+                #print(stack)
+                found = True
+                stack.pop()
+            
+            if not found: stack.append(c)
+
+        return len(''.join(stack))
