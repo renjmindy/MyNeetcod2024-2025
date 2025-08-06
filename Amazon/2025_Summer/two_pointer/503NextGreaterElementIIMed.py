b68c5812cbd48429646082baf143a899035b955b
Mindy Jen <jencmhep@gmail.com>
2025-08-05 19:48:43 -0700

    Update 503NextGreaterElementIIMed.py

--- Amazon/2025_Summer/two_pointer/503NextGreaterElementIIMed.py
+++ Amazon/2025_Summer/two_pointer/503NextGreaterElementIIMed.py
@@ -1,1 +1,12 @@
+class Solution:
+    def nextGreaterElements(self, nums: List[int]) -> List[int]:
+        
+        stack = nums[::-1]
+        ans = [-1] * len(nums)
 
+        for r in range(len(nums) - 1, -1, -1):
+            while stack and stack[-1] <= nums[r]: stack.pop()
+            if stack: ans[r] = stack[-1]
+            stack.append(nums[r])
+
+        return ans
