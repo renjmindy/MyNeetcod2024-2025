446e9b5b5d1e25f6eed59f48c56565764fd3efd7
Mindy Jen <jencmhep@gmail.com>
2025-11-01 09:24:33 -0700

    Create 3.py

---
+++ CodeSignal/Refactoring Code for Readability and Maintainability/Understanding Code Smells: Identifying the Need for Refactoring in Your Code/Unit2/3.py
@@ -0,0 +1,8 @@
+from math import factorial
+
+def factorial_of_number(num):
+    return factorial(num)
+
+number = 5
+result = factorial_of_number(number)
+print(f"The factorial of {number} is {result}")
