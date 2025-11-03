fa8ea918d748ce00f335ebd99a5762045e0e8020
Mindy Jen <jencmhep@gmail.com>
2025-11-02 17:27:16 -0800

    Create 2.py

---
+++ CodeSignal/Refactoring Code for Readability and Maintainability/Understanding Code Smells: Identifying the Need for Refactoring in Your Code/Unit5/2.py
@@ -0,0 +1,112 @@
+class Player:
+    def __init__(self, name, power, monsters):
+        self.name = name
+        self.power = power
+
+class Monster:
+    def __init__(self, score=0, impact=0, monster=0):
+        self.score = score
+        self.impact = impact
+        self.monter = monster
+    def score_change(self, player):
+        pass
+    def compute_game_score(self):
+        pass
+    def calculate_total_impact(self, player):
+        pass
+        
+class Orc(Monster):
+    def __init__(self, score, impact, strength):
+        self.score = score
+        self.impact = impact
+        self.strength = strength
+    def score_change(self, player):
+        if player.power > self.strength:
+            return player.power - self.strength
+        else:
+            return self.strength - player.power
+    def compute_game_score(self):
+        self.score += self.score_change(player)
+        return self.score
+    def calculate_total_impact(self):
+        if player.power > self.strength:
+            self.impact += player.power - self.strength
+        else:
+            self.impact -= self.strength - player.power
+        return self.impact
+
+#    def score_change(self, monster):
+#        if self.power > monster:
+#            return self.power - monster
+#        else:
+#            return monster - self.power
+        
+#    def compute_game_score(self):
+#        score = 0
+#        for monster in self.monsters:
+#            if monster['type'] == 'orc':
+#                score += self.score_change(monster['strength'])
+#            elif monster['type'] == 'dragon':
+#                score += self.score_change(monster['toughness'])
+#        return score
+
+#    def calculate_total_impact(self):
+#        total_impact = 0
+#        for monster in self.monsters:
+            # Each monster is now a dictionary with 'type' and 'strength' or 'toughness' key
+#            if monster['type'] == 'orc':
+#                if self.power > monster['strength']:
+#                    total_impact += self.power - monster['strength']
+#                else:
+#                    total_impact -= monster['strength'] - self.power
+#            elif monster['type'] == 'dragon':
+#                if self.power > monster['toughness']:
+#                    total_impact += (self.power - monster['toughness'] * 2)
+#                else:
+#                    total_impact -= (monster['toughness'] * 2 - self.power)
+#        return total_impact
+
+# Example usage
+monsters = [{'type': 'orc', 'strength': 100}, {'type': 'dragon', 'toughness': 150}, {'type': 'orc', 'strength': 80}]
+player = Player("John", 120)
+
+total_impact = player.calculate_total_impact()
+print(f"Total Combat Impact: {total_impact}")
+
+    def score_change(self, monster):
+        if self.power > monster:
+            return self.power - monster
+        else:
+            return monster - self.power
+        
+    def compute_game_score(self):
+        score = 0
+        for monster in self.monsters:
+            if monster['type'] == 'orc':
+                score += self.score_change(monster['strength'])
+            elif monster['type'] == 'dragon':
+                score += self.score_change(monster['toughness'])
+        return score
+
+    def calculate_total_impact(self):
+        total_impact = 0
+        for monster in self.monsters:
+            # Each monster is now a dictionary with 'type' and 'strength' or 'toughness' key
+            if monster['type'] == 'orc':
+                if self.power > monster['strength']:
+                    total_impact += self.power - monster['strength']
+                else:
+                    total_impact -= monster['strength'] - self.power
+            elif monster['type'] == 'dragon':
+                if self.power > monster['toughness']:
+                    total_impact += (self.power - monster['toughness'] * 2)
+                else:
+                    total_impact -= (monster['toughness'] * 2 - self.power)
+        return total_impact
+
+# Example usage
+monsters = [{'type': 'orc', 'strength': 100}, {'type': 'dragon', 'toughness': 150}, {'type': 'orc', 'strength': 80}]
+player = Player("John", 120, monsters)
+
+total_impact = player.calculate_total_impact()
+print(f"Total Combat Impact: {total_impact}")
